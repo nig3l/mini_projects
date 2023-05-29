14bbac7d7755b005ff036171ed999f6fdd0b2d20
nigel <nigelchimwene@gmail.com>
2023-05-29 12:40:44 +0300

    v3

--- MMM.py
+++ MMM.py
@@ -21,18 +21,18 @@
 
 # mode
 
-list1 = [12, 16, 20, 20, 12, 30, 25, 23, 24, 20]
+# list1 = [12, 16, 20, 20, 12, 30, 25, 23, 24, 20]
 
-frequency = {}
-for i in list1:
-    frequency.setdefault(i, 0)
-    frequency[i]+=1
+# frequency = {}
+# for i in list1:
+#     frequency.setdefault(i, 0)
+#     frequency[i]+=1
 
-frequent = max(frequency.values())
-for i, j in frequency.items():
-    if j == frequent:
-        mode = i
+# frequent = max(frequency.values())
+# for i, j in frequency.items():
+#     if j == frequent:
+#         mode = i
 
-print(mode)
+# print(mode)
 
 
