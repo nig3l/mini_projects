212d8e62cb49f2e1bfefe7631db6ad5bf2fab875
nigel <nigelchimwene@gmail.com>
2023-05-30 10:52:56 +0300

    v5

---
+++ reversestrings.py
@@ -0,0 +1,8 @@
+def reverse_string(string):
+    return string[::-1]
+
+string = "hito hito no mi"
+print(reverse_string(string))
+
+#The string slicing operator “::” reads all the characters of the string, 
+# and -1, in the end, reverses the order of the characters
