ac7ffaf584d8422eaa79a0b96fae10a985f83001
nigel <nigelchimwene@gmail.com>
2023-06-17 16:05:31 +0300

    testing

--- Automation/webscraping.py
+++ Automation/webscraping.py
@@ -1,13 +1,17 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+# import requests
+
+# response = requests.get("https://www.britannica.com")
+
+# soup = BeautifulSoup(response.content,'html.parser')
+
+# # print(soup.find_all('h1'))
+
+# # finding all the links
+# for link in soup.find_all('a'): 
+#     print(link.get('href'))
+
 import requests
-
-response = requests.get("https://www.britannica.com")
-
-soup = BeautifulSoup(response.content,'html.parser')
-
-# print(soup.find_all('h1'))
-
-# finding all the links
-for link in soup.find_all('a'): 
-    print(link.get('href'))
-
+URL = "https://www.geeksforgeeks.org/data-structures/"
+r = requests.get(URL)
+print(r.content)
