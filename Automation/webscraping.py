0bd635a28a1666a42c70e444bb4ea9afec0b9951
nigel <nigelchimwene@gmail.com>
2023-06-18 14:19:06 +0300

    quickfix

--- Automation/webscraping.py
+++ Automation/webscraping.py
@@ -1,17 +1,26 @@
-# from bs4 import BeautifulSoup
-# import requests
-
-# response = requests.get("https://www.britannica.com")
-
-# soup = BeautifulSoup(response.content,'html.parser')
-
-# # print(soup.find_all('h1'))
-
-# # finding all the links
-# for link in soup.find_all('a'): 
-#     print(link.get('href'))
-
+from bs4 import BeautifulSoup
 import requests
-URL = "https://www.geeksforgeeks.org/data-structures/"
-r = requests.get(URL)
-print(r.content)
+url = "https://realpython.github.io/fake-jobs/"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content,'html.parser')
+print(page.text)
+
+
+
+
+
+'''
+from bs4 import BeautifulSoup
+ import requests
+
+ response = requests.get("https://www.britannica.com")
+
+ soup = BeautifulSoup(response.content,'html.parser')
+
+ print(soup.find_all('h1'))
+
+ # finding all the links
+ for link in soup.find_all('a'): 
+     print(link.get('href'))
+'''
