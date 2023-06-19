04a7d1d91c3149cf4eeb8c37f8bc646e67d1a7c2
nigel <nigelchimwene@gmail.com>
2023-06-19 15:04:41 +0300

    -

--- scripts/randomnumber.py
+++ scripts/randomnumber.py
@@ -1,21 +1,28 @@
-import random
-n = random.randrange(1,10)
-guess = int(input("entre number :"))
-while n != guess:
-    if guess < n:
-        print("too low")
-        guess = int(input("entre number :"))
+# import random
+# n = random.randrange(1,10)
+# guess = int(input("entre number :"))
+# while n != guess:
+#     if guess < n:
+#         print("too low")
+#         guess = int(input("entre number :"))
 
-    elif guess > n:
-        print("too high")
-        guess = int(input("entre number :"))
+#     elif guess > n:
+#         print("too high")
+#         guess = int(input("entre number :"))
 
         
 
-    else:
-        break
+#     else:
+#         break
 
-print('you guessed the right number')
+# print('you guessed the right number')
 
+'''
+
+------------------------version 2----------guessing game
+
+'''
+
+import random
 
 
