bc28eab6217de7c7cfa302916b45e44df6d173cb
nigel <nigelchimwene@gmail.com>
2023-06-25 14:46:49 +0300

    file handling

--- Automation/filehandling.py
+++ Automation/filehandling.py
@@ -1,5 +1,6 @@
+# test1
 
-
+'''
 # Creating and Writing to a File
 file_name = 'bot.txt'
 content = "assistant bot commands and maintenance"
@@ -25,3 +26,29 @@ with open(file_name, "r") as file:
     read_content = file.read()
     print(read_content)
 
+'''
+
+# test2
+
+file_name = "networksockets.txt"
+content = "RouterInstructions"
+
+with open(file_name, "w") as file:
+    file.write(content)
+
+# reading line by line
+
+with open(file_name, "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        print(line.strip())  # Strip newline character
+
+additional_content = "\nConfirm your IP address."
+
+with open(file_name, "a") as file:
+    file.write(additional_content)
+
+with open(file_name, "r") as file:
+    read_content = file.read()
+    print(read_content)
+    
