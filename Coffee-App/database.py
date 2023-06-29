41ecb8ed5eaa976e061962655b216e9504d2d513
nigel <nigelchimwene@gmail.com>
2023-06-29 12:32:29 +0300

    tables

--- Coffee-App/database.py
+++ Coffee-App/database.py
@@ -1,17 +1,32 @@
 import sqlite3
 
+
+CREATE_BEANS_TABLE = "CREATE TABLE beans( id INTEGER PRIMARY KEY, name TEXT, method TEXT, rating INTEGER);"
+
+INSERT_BEAN = "INSERT INTO beans(name , method, rating)VALUES(?, ?, ?);"
+
+GET_ALL_BEANS = "SELECT * FROM beans;"
+GET_BEANS_BY_NAME = "SELECT * FROM beans WHERE name = ? "
+
 def connect():
     return sqlite3.connect("data.db")
 
 
 def create_tables(connection):
     with connection:
-     connection.execute(""" CREATE TABLE beans(
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    method TEXT,
-    rating INTEGER
-    
-    )  
-    """)
+     connection.execute(CREATE_BEANS_TABLE)
 
+def add_bean(connection,name,method,rating):
+   with connection:
+      connection.execute(INSERT_BEAN, (name,method,rating))
+
+def get_all_beans(connnection):
+   with connnection:
+      connnection.execute(GET_ALL_BEANS)
+
+def get_all_beans_by_name(connection,name):
+   with connection:
+      connection.execute(GET_BEANS_BY_NAME,(name,))
+   
+
+      
