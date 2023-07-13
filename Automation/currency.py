c4646f295d1adc02833968ee93e82ab5c572fa0b
nigel <nigelchimwene@gmail.com>
2023-07-14 00:28:36 +0300

    currency converter

--- Automation/currency.py
+++ Automation/currency.py
@@ -9,5 +9,23 @@ def convert_currency(amount, from_currency, to_currency):
     data = response.json()
 
     if response.status_code == 200:
-        exchange_rate = data["rates"][to_currency]
+        exchange_rates = data["rates"]
+        exchange_rate = exchange_rates[to_currency]
+
+        converted_amount = amount * exchange_rate
+
+        return converted_amount
+    else:
+        # If the API request fails, print an error message
+        print("Unable to retrieve exchange rates.")
+        return None
+    
+# Example usage
+amount = 100  # Amount to convert
+from_currency = "USD"  # Currency to convert from
+to_currency = "EUR"  # Currency to convert to
+
+converted_amount = convert_currency(amount, from_currency, to_currency)
+if converted_amount is not None:
+    print(f"{amount} {from_currency} is equal to {converted_amount} {to_currency}.")
 
