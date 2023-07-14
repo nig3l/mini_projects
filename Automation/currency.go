2891412d09f5294e6b5a5072b9ffe2612e1dd0de
nigel <nigelchimwene@gmail.com>
2023-07-14 13:25:26 +0300

    currency

--- Automation/currency.go
+++ Automation/currency.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "net/http"
+  "io/ioutil"
 )
 
 func currencyConverter(amount float64, fromCurrency string, toCurrency string) float64 {
@@ -19,4 +20,23 @@ func currencyConverter(amount float64, fromCurrency string, toCurrency string) f
   if err != nil {
     panic(err)
   }
+
+  exchangeRate := rates[toCurrency]
+
+    // Convert the amount.
+	convertedAmount := amount * exchangeRate
+
+	return convertedAmount
+  }
+
+  func main() {
+	amount := 100
+	fromCurrency := "USD"
+	toCurrency := "EUR"
+	convertedAmount := currencyConverter(amount, fromCurrency, toCurrency)
+	fmt.Printf("%.2f %s is equal to %.2f %s\n", amount, fromCurrency, convertedAmount, toCurrency)
+  }
+
+  
+
 
