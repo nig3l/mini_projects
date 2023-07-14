58bd20c9c3379527ece8e52ba21250aec4e50223
nigel <nigelchimwene@gmail.com>
2023-07-15 01:35:53 +0300

    basic program structure

--- GO/basics/intro.go
+++ GO/basics/intro.go
@@ -1,3 +1,5 @@
+// prog1
+
 package main
 
 import "fmt"
@@ -6,4 +8,23 @@ func main(){
 
 	fmt.Println("yeah")
 }
+
+// prog2  Check if a Number is Even or Odd
+
+package main
+
+import "fmt"
+
+func main(){
+
+	// num1 := 
+
+	if num%2 == 0 {
+		fmt.Println("Even")
+	} else {
+		fmt.Println("Odd")
+	}
+}
+
+// prog3 fibonacci series
 
